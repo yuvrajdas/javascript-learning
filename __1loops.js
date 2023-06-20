4dcf4339929f1b17870e6858812a5b1d3712ea53
yuvrajdas <yuvrajdas1531@gmail.com>
2023-06-20 07:28:24 +0530

    loops

---
+++ __1loops.js
@@ -0,0 +1,31 @@
+/*  loops are used to print the single statement in multiple time
+    There are 5 types of loops in JavaScript
+
+    1. for(initial state; condition; increment/decrement) { body of the loop/your logic works}
+    2. do while loop
+    3. while
+    4. for in
+    5. for of
+*/
+
+//for loop
+let i =0 ; // initial value
+for(i; i<5; i++){
+    console.log("foor loop",i);
+}
+console.log("---------------------------------------");
+
+//do-while Loop
+let j=6;//initial value
+do{
+    console.log("do while value",j);
+    j--;
+}while(j>2);
+    
+console.log("----------------------------------------")
+//While Loops
+let k=7;
+while(k>0){
+    console.log("while loop value", k);
+    k--;
+}
